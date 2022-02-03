a8a782949192ab375f15549c3f508603b916f56d
roman semkin <roman.semkin@ssh.com>
2022-02-03 22:29:26 +0200

    Squashed 'testSBTR/test/' changes from 393f551..d7aedf8
    d7aedf8 Initial commit
    REVERT: 393f551 Init container was added
    REVERT: ac40a26 Initial commit
    
    git-subtree-dir: testSBTR/test
    git-subtree-split: d7aedf88cb4050bed1388a6940d4ed3e4c34d8b6

--- deploy/postgres/docker-entrypoint-initdb.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "host replication all 0.0.0.0/0 md5" >> "$PGDATA/pg_hba.conf"
-set -e
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-CREATE USER $POSTGRES_STANDBY_USER REPLICATION LOGIN ENCRYPTED PASSWORD '$POSTGRES_STANDBY_PASSWORD';
-EOSQL
-
-exec "$@"
